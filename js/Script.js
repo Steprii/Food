56e7363696198b34920fcdf51a74961fa3711c2d
Andrey <stepanov.andrii.public@gmail.com>
2023-06-22 23:33:59 +0300

    Add Adaptiv

--- js/Script.js
+++ js/Script.js
@@ -41,58 +41,58 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Timer
 
-    const deadline = '2022-06-11';
-
-    function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor( (t/(1000*60*60*24)) ),
-            seconds = Math.floor( (t/1000) % 60 ),
-            minutes = Math.floor( (t/1000/60) % 60 ),
-            hours = Math.floor( (t/(1000*60*60) % 24) );
-
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-
-    function getZero(num){
-        if (num >= 0 && num < 10) {
-            return '0' + num;
-        } else {
-            return num;
+        const deadline = '2022-06-11';
+    
+        function getTimeRemaining(endtime) {
+            const t = Date.parse(endtime) - Date.parse(new Date()),
+                days = Math.floor( (t/(1000*60*60*24)) ),
+                seconds = Math.floor( (t/1000) % 60 ),
+                minutes = Math.floor( (t/1000/60) % 60 ),
+                hours = Math.floor( (t/(1000*60*60) % 24) );
+    
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
         }
-    }
-
-    function setClock(selector, endtime) {
-
-        const timer = document.querySelector(selector),
-            days = timer.querySelector("#days"),
-            hours = timer.querySelector('#hours'),
-            minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-
-        updateClock();
-
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
-
-            days.innerHTML = getZero(t.days);
-            hours.innerHTML = getZero(t.hours);
-            minutes.innerHTML = getZero(t.minutes);
-            seconds.innerHTML = getZero(t.seconds);
-
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
+    
+        function getZero(num){
+            if (num >= 0 && num < 10) {
+                return '0' + num;
+            } else {
+                return num;
             }
         }
-    }
-
-    setClock('.timer', deadline);
+    
+        function setClock(selector, endtime) {
+    
+            const timer = document.querySelector(selector),
+                days = timer.querySelector("#days"),
+                hours = timer.querySelector('#hours'),
+                minutes = timer.querySelector('#minutes'),
+                seconds = timer.querySelector('#seconds'),
+                timeInterval = setInterval(updateClock, 1000);
+    
+            updateClock();
+    
+            function updateClock() {
+                const t = getTimeRemaining(endtime);
+    
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+    
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                }
+            }
+        }
+    
+        setClock('.timer', deadline);
 
     // Modal
 
@@ -129,7 +129,6 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     const modalTimerId = setTimeout(openModal, 50000);
-    // Изменил значение, чтобы не отвлекало
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -260,7 +259,7 @@ window.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
-
+    
     function showThanksModal(message) {
         const prevModalDialog = document.querySelector('.modal__dialog');
 
